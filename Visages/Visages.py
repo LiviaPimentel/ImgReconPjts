23f8e1913e197c5055807f23e8dab3107f2735d6
Livia Fragoso Pimentel <35858071+LiviaPimentel@users.noreply.github.com>
2020-12-22 17:03:42 -0300

    Add files via upload

---
+++ Visages/Visages.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+import cv2 as cv
+import argparse
+
+
+def detectAndDisplay(frame):    
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame_gray = cv.equalizeHist(frame_gray)
+    #-- Detect faces
+    faces = face_cascade.detectMultiScale(frame_gray)
+    for (x,y,w,h) in faces:
+        center = (x + w//2, y + h//2)
+        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+        faceROI = frame_gray[y:y+h,x:x+w]
+    cv.imshow('Capture - Face detection', frame)
+
+face_cascade_name = 'haarcascade_frontalface_alt.xml'
+face_cascade = cv.CascadeClassifier()
+
+if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
+    print('--(!)Error loading face cascade')
+    exit(0)
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened:
+    print('--(!)Error opening video capture')
+    exit(0)
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        print('--(!) No captured frame -- Break!')
+        break
+    detectAndDisplay(frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv.destroyAllWindows() 
